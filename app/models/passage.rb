0b63f98b17493092455751d591ff210c2907fe1d
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-03-19 14:51:23 -0700

    Add FK relations (#2)

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Passage < ApplicationRecord
+  belongs_to :article
+  has_many :patterns, dependent: :delete_all
 end
--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Pattern < ApplicationRecord
+  belongs_to :passage
 end
