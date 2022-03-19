e1db1951d4b0226d2e3489a859fcee7932af35b4
Justin Chang <changguangyu@yahoo.com>
2022-03-19 13:45:54 -0700

    Init

---
+++ app/models/worker.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Worker < ApplicationRecord
+end
---
+++ db/migrate/20220319202352_create_workers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Create workers model
+class CreateWorkers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :workers do |t|
+      t.string :worker_id
+      t.integer :quiz_attempts
+
+      t.timestamps
+    end
+  end
+end
