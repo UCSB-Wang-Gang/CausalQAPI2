51f989a374d37ae635a19a13d70204ee1f230082
Justin Chang <changguangyu@yahoo.com>
2022-03-28 22:50:56 -0700

    Add endpoint to reset submissions since last check count

---
+++ app/controllers/api/worker_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  # Controller for Workers
+  class WorkerController < ApplicationController
+    def reset_last_check
+      worker = Worker.find_by(worker_id: worker_params[:worker_id])
+      if worker.present?
+        worker.submissions_since_check = 0
+        worker.save
+        render json: worker
+      else
+        render json: { error: 'Worker not found' }, status: :not_found
+      end
+    end
+
+    private
+
+    def worker_params
+      params.require(:worker).permit(:worker_id)
+    end
+  end
+end
