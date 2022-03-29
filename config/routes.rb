9c9e51aea6218f35f5a6d1d110599a401d664ef5
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-03-28 22:51:43 -0700

    Add endpoint to reset submissions since last check count (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post '/add_hit', action: :add_hit, controller: :hit
+
+    post '/reset_last_check', action: :reset_last_check, controller: :worker
   end
 end
