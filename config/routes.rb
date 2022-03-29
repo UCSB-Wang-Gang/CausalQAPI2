dfb244172f735adad3712e793b12d59b4dc8f0a1
Matthew Ho <msho@ucsb.edu>
2022-03-28 23:56:22 -0700

    got hit (ouch)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post '/add_hit', action: :add_hit, controller: :hit
+    get '/get_hits/:worker_id', action: :worker_hits, controller: :hit
 
     post '/reset_last_check', action: :reset_last_check, controller: :worker
   end
