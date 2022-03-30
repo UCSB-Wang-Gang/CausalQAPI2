3ee5bb30cb0c6df3ea7099b5fc1a5bf54a0dccd9
Justin Chang <changguangyu@yahoo.com>
2022-03-29 19:12:58 -0700

    Qualify worker

--- app/controllers/api/worker_controller.rb
+++ app/controllers/api/worker_controller.rb
@@ -15,7 +15,7 @@ module Api
     end
 
     def review_top_k
-      workers = Worker.order(params['criteria'] + " DESC").limit(params['num_workers'])
+      workers = Worker.order("#{params['criteria']} DESC").limit(params['num_workers'])
       if workers.present?
         render json: workers
       else
@@ -23,10 +23,29 @@ module Api
       end
     end
 
+    def qualify_worker
+      client = Aws::MTurk::Client.new(region: 'us-east-1')
+      worker = create_worker(worker_params[:worker_id])
+      return render json: { error: 'Worker already exists' } if worker.present?
+
+      render json: client.associate_qualification_with_worker({
+                                                                qualification_type_id: 'EntityId',
+                                                                worker_id: worker.worker_id,
+                                                                integer_value: 1,
+                                                                send_notification: false
+                                                              })
+    end
+
     private
 
     def worker_params
       params.require(:worker).permit(:worker_id)
+    end
+
+    def create_worker(worker_id)
+      worker = Worker.find_by(worker_id: worker_id)
+      worker = Worker.create(worker_id: worker_id) unless worker.present?
+      worker
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@
 
 Rails.application.routes.draw do
   namespace :api do
-    post '/add_hit', action: :add_hit, controller: :hit
     get '/get_hits/:worker_id', action: :worker_hits, controller: :hit
+    post '/add_hit', action: :add_hit, controller: :hit
 
-    post '/reset_last_check', action: :reset_last_check, controller: :worker
     get '/get_top_k/:num_workers/:criteria', action: :review_top_k, controller: :worker
+    post '/qualify_worker', action: :qualify_worker, controller: :worker
+    post '/reset_last_check', action: :reset_last_check, controller: :worker
 
-    post '/add_passage', action: :add_passage, controller: :passage
     get '/get_passage', action: :return_passage, controller: :passage
+    post '/add_passage', action: :add_passage, controller: :passage
   end
 end
