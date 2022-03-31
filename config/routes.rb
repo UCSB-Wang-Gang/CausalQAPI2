993ec4b786cb5520369d03874951ea432f075861
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-03-31 13:03:10 -0700

    Check worker qualification endpoint (#15)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     post '/reset_last_check', action: :reset_last_check, controller: :worker
     post '/qualify_worker', action: :qualify_worker, controller: :worker
     get '/get_top_k/:num_workers/:criteria', action: :review_top_k, controller: :worker
+    get '/worker_qualification/:worker_id', action: :worker_qualification, controller: :worker
 
     post '/add_passage', action: :add_passage, controller: :passage
     get '/get_passage', action: :return_passage, controller: :passage
