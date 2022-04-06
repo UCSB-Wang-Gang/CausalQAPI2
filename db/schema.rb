3bfd1fca385779e9c4acf28e3d2a50d50a40ca4e
msho-student <72837834+msho-student@users.noreply.github.com>
2022-04-05 23:05:30 -0700

    Passage delete threshold (#27)
    * basic seeding
    
    * better seeds
    
    * changed get passage endpoint to only delete after a passage has been skipped over some number of times; made add hit endpoint delete the passage as well

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_070757) do
+ActiveRecord::Schema.define(version: 2022_04_06_044457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_070757) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "article_id"
     t.string "patterns", default: ""
+    t.integer "retrieved", default: 0
     t.index ["article_id"], name: "index_passages_on_article_id"
   end
 
