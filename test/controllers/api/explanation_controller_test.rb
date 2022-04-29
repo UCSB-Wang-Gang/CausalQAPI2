e6bce99bf321000cd6f88dc175c1dc13bb377d20
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-04-29 14:45:45 -0700

    Add logic for creating explanations (#32)

---
+++ test/controllers/api/explanation_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Api
+  class ExplanationControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
