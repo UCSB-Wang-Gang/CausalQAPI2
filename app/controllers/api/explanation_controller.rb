986b60d45d5d6d4deaa8ad33c220e1617d976728
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-04-29 14:45:45 -0700

    Add logic for creating explanations (#32)

---
+++ app/controllers/api/explanation_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Api
+  # Controller for stage 2 explanations
+  class ExplanationController < ApplicationController
+    def add_explanation
+      worker = Worker.find_by(worker_id: hit_params[:worker_id])
+      return render json: { error: 'worker not found' } unless worker.present?
+      return render json: { error: 'assignment_id already exists' } if matched_ass
+
+      explanation = create_explanation(worker)
+      increase_submission_count(worker)
+      render json: explanation
+    end
+
+    private
+
+    def increase_submission_count(worker)
+      worker.submissions = worker.submissions + 1
+      worker.checked_status = 'limited' if (worker.submissions >= 20) && (worker.checked_status == 'unchecked')
+      worker.submissions_since_check = worker.submissions_since_check + 1
+      worker.save
+    end
+
+    def create_explanation(worker)
+      Explanation.create(
+        worker_id: worker.id,
+        hit_id: hit_params[:hit_id],
+        assignment_id: hit_params[:assignment_id],
+        explanation: hit_params[:explanation]
+      )
+    end
+
+    def explanation_params
+      params.require(:explanation).permit(:explanation, :worker_id, :hit_id, :assignment_id)
+    end
+  end
+end
