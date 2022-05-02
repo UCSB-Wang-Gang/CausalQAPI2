310b840594f1d2728ced3654b1832d1e9f4d0d13
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-05-02 00:19:27 -0700

    Get worker explanations (#36)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_060106) do
+ActiveRecord::Schema.define(version: 2022_04_29_225155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_04_22_060106) do
     t.bigint "article_id"
     t.string "patterns", default: ""
     t.integer "retrieved", default: 0
+    t.string "state", default: "unchecked"
     t.index ["article_id"], name: "index_passages_on_article_id"
   end
 
