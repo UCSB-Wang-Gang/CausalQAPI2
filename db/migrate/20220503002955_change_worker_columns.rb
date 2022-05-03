340a71eaec0239de4e3cc9bb2bc17f726c47983c
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-05-02 17:48:55 -0700

    Change column types from  string to int (#41)
    * Change column types from string to int
    
    * Lint

---
+++ db/migrate/20220503002955_change_worker_columns.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Change column data type for workers
+class ChangeWorkerColumns < ActiveRecord::Migration[6.1]
+  def change
+    change_column :workers, :explanation_submits, :integer, using: 0
+    change_column :workers, :explanations_since_check, :integer, using: 0
+  end
+end
